2d8921dbe5b8cfa6e2d5e321a53c38b0bb6ef90c
Marcel Fernandez Rosas <60543555+Ijusttyped@users.noreply.github.com>
2022-06-24 16:43:45 +0200

    Tests for clean_feature package

--- test/resources/sample_data.py
+++ test/resources/sample_data.py
@@ -2,61 +2,92 @@
 from datetime import datetime
 
 import pandas as pd
+import numpy as np
+
+from utils.data_models import CleanedFeaturesSchema
 
 
 RAW_DATA_1 = pd.DataFrame(
     {
-        "Company size": ["100+", "1-10"],
-        "Current Salary": [100000.0, 50000.0],
-        "Age": [24.0, 40.0],
-        "Gender": ["male", "female"],
-        "City": ["Berlin", "Cologne"],
-        "Seniority": ["Mid", "Junior"],
-        "Position": ["Developer", "Engineer"],
-        "Total years of experience": [4, 20],
-        "Company type": ["Product", "Startup"],
-        "Timestamp": ["2020/01/01", "2020/09/15"],
+        "Company size": ["101-1000", "1-100", np.NaN],
+        "Current Salary": [100000.0, 50000.0, np.NaN],
+        "Age": [24.0, 40.0, np.NaN],
+        "Gender": ["male", "female", np.NaN],
+        "City": ["Berlin", "Cologne", np.NaN],
+        "Seniority": ["Mid", "Junior", np.NaN],
+        "Position": ["Developer", "Engineer", np.NaN],
+        "Total years of experience": [8.0, 20.0, np.NaN],
+        "Company type": ["Product", "Startup", np.NaN],
+        "Timestamp": ["2020/01/01", "2020/09/15", np.NaN],
     }
 )
 
 RAW_DATA_2 = pd.DataFrame(
     {
-        "Zeitstempel": ["2022/01/22", "2021/08/29"],
-        "Company size": ["100", "50-100"],
-        "Current Salary": [55000.0, 65000.0],
-        "Age": [34.0, 33.0],
-        "Gender": ["diverse", "female"],
-        "City": ["Berlin", "Cologne"],
-        "Seniority": ["Senior", "Mid"],
-        "Position": ["Data Scientist", "Manager"],
-        "Total years of experience": [10, 9],
-        "Company type": ["Consulting", "Product"],
+        "Zeitstempel": ["2022/01/22", "2021/08/29", "2021/08/29"],
+        "Company size": ["11-50", "50-100", "50-100"],
+        "Current Salary": [55000.0, 65000.0, 65000.0],
+        "Age": [34.0, 33.0, 33.0],
+        "Gender": ["diverse", "female", "female"],
+        "City": ["Berlin", "Cologne", "Cologne"],
+        "Seniority": ["Senior", "Mid", "Mid"],
+        "Position": ["Data Scientist", "Manager", "Manager"],
+        "Total years of experience": [10.0, 9.0, 9.0],
+        "Company type": ["Consulting", "Product", "Product"],
     }
 )
 
 RAW_DATA_COMBINED = pd.DataFrame(
     {
-        "Company_Size": ["100+", "1-10", "100", "50-100"],
-        "Salary_Yearly": [100000.0, 50000.0, 55000.0, 65000.0],
-        "Age": [24.0, 40.0, 34.0, 33.0],
-        "Gender": ["male", "female", "diverse", "female"],
-        "City": ["Berlin", "Cologne", "Berlin", "Cologne"],
-        "Seniority": ["Mid", "Junior", "Senior", "Mid"],
+        "Company_Size": ["101-1000", "1-100", np.NaN, "11-50", "50-100", "50-100"],
+        "Salary_Yearly": [100000.0, 50000.0, np.NaN, 55000.0, 65000.0, 65000.0],
+        "Age": [24.0, 40.0, np.NaN, 34.0, 33.0, 33.0],
+        "Gender": ["male", "female", np.NaN, "diverse", "female", "female"],
+        "City": ["Berlin", "Cologne", np.NaN, "Berlin", "Cologne", "Cologne"],
+        "Seniority": ["Mid", "Junior", np.NaN, "Senior", "Mid", "Mid"],
         "Position": [
             "Developer",
             "Engineer",
+            np.NaN,
             "Data Scientist",
             "Manager",
+            "Manager",
         ],
-        "Years_of_Experience": ["4", "20", "10", "9"],
-        "Company_Type": ["Product", "Startup", "Consulting", "Product"],
+        "Years_of_Experience": ["8.0", "20.0", np.NaN, "10.0", "9.0", "9.0"],
+        "Company_Type": [
+            "Product",
+            "Startup",
+            np.NaN,
+            "Consulting",
+            "Product",
+            "Product",
+        ],
         "Timestamp": [
             datetime(2020, 1, 1),
             datetime(2020, 9, 15),
+            np.NaN,
             datetime(2022, 1, 22),
+            datetime(2021, 8, 29),
             datetime(2021, 8, 29),
         ],
     }
+)
+
+CLEANED_FEATURES = CleanedFeaturesSchema(
+    pd.DataFrame(
+        {
+            "Company_Size": ["101-1000", "1-100", "1-100", "1-100"],
+            "Age": [24, 40, 34, 33],
+            "Gender": ["male", "female", "diverse", "female"],
+            "City": ["berlin", "cologne", "berlin", "cologne"],
+            "Seniority": ["mid", "junior", "senior", "mid"],
+            "Position": ["software developer", "engineer", "data scientist", "manager"],
+            "Years_of_Experience": [6.0, 20.0, 10.0, 9.0],
+            "Company_Type": ["product", "startup", "consulting or agency", "product"],
+            "Year": [2020, 2020, 2022, 2021],
+        },
+        index=pd.Index([0, 1, 3, 4]),
+    )
 )
 
 FEATURES = pd.DataFrame(
